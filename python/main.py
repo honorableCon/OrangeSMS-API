376255db77cd59d30eb87cf60b1005a21b30cacb
Honorable con <honorablecon@gmail.com>
2022-01-04 11:53:23 +0000

    python:adding send function

--- python/main.py
+++ python/main.py
@@ -1,8 +1,7 @@
+import json
 import requests
 
-clientID = "{{idclient}}"
-senderAdress = "+2210000"
-address = "+221{{receiver}}"
+clientID="{{clientID}}"
 
 def getToken(clientID):
     response = requests.post('https://api.orange.com/oauth/v3/token', 
@@ -16,3 +15,49 @@ def getToken(clientID):
     )
     response = response.json()
     return response['access_token']
+
+
+def getUsageStats(token):
+    response = requests.get('https://api.orange.com/sms/admin/v1/statistics',
+        headers={
+            'Authorization': f'Bearer {token}'
+        }
+    )
+    stats = response.json()
+
+    return stats
+
+
+def showBalanceSMS(token):
+    response = requests.get('https://api.orange.com/sms/admin/v1/contracts', 
+        headers={
+            'Authorization': f"Bearer {token}"
+        }
+    )
+    balance = response.json()
+    print(balance)
+
+    return balance
+
+
+def sendSMS(senderAddress, receiverAddress, message, token):
+    url = f'https://api.orange.com/smsmessaging/v1/outbound/tel:{senderAddress}/requests'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        "outboundSMSMessageRequest":{
+            "address": f"tel:{receiverAddress}",
+            "senderAddress":f"tel:{senderAddress}",
+            "outboundSMSTextMessage":{
+                "message": message
+        }     
+    }}
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    return response.json()
+
+token = getToken(clientID)
+# code here
